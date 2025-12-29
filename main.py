4f59512a7cfadf538d0a5ce72f6ca743caa2b2dc
Responsiblelife69 <154543768+Responsiblelife69@users.noreply.github.com>
2025-12-29 15:02:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+from src.f1_data import get_race_telemetry, enable_cache, get_circuit_rotation, load_session, get_quali_telemetry
+from src.arcade_replay import run_arcade_replay
+from src.interfaces.qualifying import run_qualifying_replay
+import sys
+
+def main(year = None, round_number = None, playback_speed =1, session_type = 'R'):
+    print(f"Loading F1 {year} Round {round_number} session '{session_type}")
+    session = load_session(year, round_number, session_type)
+    print(f"Loaded session: {session.event['EventName']} - {session.event['RoundNumber']} - {session_type}")
+    enable_cache()
+    if session_type == 'Q' or session_type == 'SQ':
+        qualifying_session_data = get_quali_telemetry(session, session_type = session_type)
+        title = f"{session.event['EventName']} - {'Sprint Qualifying' if session_type == 'SQ' else 'Qualifying Result'}"
+        run_qualifying_replay(
+            session=session,
+            data = qualifying_session_data,
+            title=title,
+        )
+
+    else:
+        race_telemetry = get_race_telemetry(session, session_type=session_type)
+        example_lap = session.laps.pick_fastest().get_telemetry()
+        driver = session.drivers
+        circuit_rotation = get_circuit_rotation(session)
+        chart = "--chart" in sys.argv
+        run_arcade_replay(
+            frames = race_telemetry['frames'],
+            track_statuses = race_telemetry['track_statuses'],
+            example_lap = example_lap,
+            drivers = driver,
+            playback_speed = 1.0,
+            driver_colors = race_telemetry['driver_colors'],
+            title = f"{session.event['EventName']} - { 'sprint' if session_type == 'S' else 'Race'} ",
+            total_laps = race_telemetry['total_laps'],
+            circuit_rotation = circuit_rotation,
+            chart = chart,
+        )
+if __name__ == "__main__":
+    if "--year" in sys.argv:
+        year_index = sys.argv.index("--year") + 1
+        year = int(sys.argv[year_index])
+    else:
+        year = 2025
+    
+    if "--round" in sys.argv:
+        round_index = sys.argv.index("--round") + 1
+        round_number = int(sys.argv[round_index])
+    else:
+        round_number = 12
+    
+    playback_speed = 1
+
+    session_type = 'SQ' if "--sprint-qualifying" in sys.argv else('S' if "--sprint" in sys.argv else('Q' if "--qualifying" in sys.argv else 'R'))
+
+    main(year, round_number, playback_speed, session_type=session_type)
+    
